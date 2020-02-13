b189c21e336eeac5d3ca7d0fe8d5eca6a583573b
wordian-org <wordian.org@gmail.com>
2020-02-14 02:20:42 +0500

    Part 1 Ex 3

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
-// Import stylesheets
-import "./style.css";
 
-// Write Javascript code!
-const appDiv = document.getElementById("app");
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+//This is also Wonderful Exercise solved by Shahmeer
 
-const x = 80;
-const y = 20;
-if (x >= 100  || y >= 100 || x+y >= 100) {
-  appDiv.innerHTML = `<h1>True</h1>`;
-} else {
-  appDiv.innerHTML = `<h1>False</h1>`;
-}
-
-
-const testi = document.getElementById("test");
-
-testi.innerHTML = "<h2>?</h2>"
-
-const var1 = (a,b) => {
-  if(a>b){
-  appDiv.innerHTML = "<h1> a is greater then b </h1>";
-
-  }else if (a<b){
-    appDiv.innerHTML = "<h1> b is greater then a </h1>";
-  }else{
-    appDiv.innerHTML = "<h1> a is equal to b </h1>";
+var fun = (Str,ee)=>{
+  nn = Str.length;
+  ne = "";
+  for(i = 0 ; i<nn ; i++){
+    var ne = ne.concat(ee);
   }
-}
 
-var1(11,11);
+  return ne;
+}
+var ttt = fun("Shahmeer","s");
+var ht = document.getElementById("test");
+console.log(fun("Shah","s"));
+
+console.log(nn);
+
+
+
+
+
